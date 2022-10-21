06fd01e620c9ca3454dbd82d17e522f5e72e635e
Mark Ihab <mark.ihab@getspokn.com>
2022-10-21 12:44:23 +0200

    Loan Qualifier class

--- src/chapter3/LoanQualifier.java
+++ src/chapter3/LoanQualifier.java
@@ -14,5 +14,16 @@ public class LoanQualifier {
         System.out.println("please enter number of years experience : ");
         double yearsOfExperience = scan.nextDouble();
         //Make Decision
+        if(salary >= requiredSalary){
+            if (yearsOfExperience >= requiredYear){
+                System.out.println("Congrats, you are qualified for the loan");
+            }
+            else {
+                System.out.println("Unfortunatly, You need to have at least " + requiredYear + " of experience");
+            }
+        }
+        else {
+            System.out.println("Unfortunatly, You need to earn at least " + requiredSalary +" per year to take the loan");
+        }
     }
 }
